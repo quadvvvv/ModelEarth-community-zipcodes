710ad199a89173a5b51b4aafde5bc12b2ec1fd33
yni <yn2443@columbia.edu>
2024-08-22 03:13:46 -0400

    docs: Update multiple README files and documentation for Python scripts
    - Revised the main README to improve clarity and structure.
    - Updated README files for various components of the repository.
    - Enhanced documentation for specific Python scripts to provide clearer guidance.

--- .github/scripts/run_validation_test.py
+++ .github/scripts/run_validation_test.py
@@ -6,16 +6,21 @@ if len(sys.argv) != 2:
     print("Usage: python run_validation_test.py <year>")
     sys.exit(1)
 
-year = int(sys.argv[1])  # Get the year from command-line arguments
+# Get the year from command-line arguments
+year = int(sys.argv[1])
 
-# The default values for the Constructor's arguments are relative pahts, therefore, adjusted: 
+# Define the base database path and export directory
+# These paths are relative to the root of the repository
 base_db_path = './industries/naics/duck_zipcode_db/zip_data/duck_db_manager/database/'
 export_dir = './industries/naics/US/zip'
 
+# Create an instance of the DataExporterTest
 tester = DataExporterTest(base_db_path=base_db_path, export_dir=export_dir, year=year)
-# result = tester.run_test()
-result = tester.run_test();
 
+# Run the validation test
+result = tester.run_test()
+
+# Check the result of the validation test
 if result:
     print(f"Validation succeeded for year {year}.")
     sys.exit(0)  # Exit with 0 for success
